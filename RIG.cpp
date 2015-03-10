14a9e215dc2e7e3f559db1004867bed1ce89dfec
Kyle Cesare <kcesare@gmail.com>
2015-03-09 23:05:27 -0700

    Add skeleton RIG.

--- RIG.cpp
+++ RIG.cpp
@@ -1,1 +1,16 @@
 #include "RIG.h"
+
+RIG::RIG(const ompl::base::SpaceInformationPtr &si) : ompl::base::Planner(si, "RIG")
+{
+}
+
+RIG::~RIG()
+{
+}
+
+ompl::base::PlannerStatus RIG::solve(const ompl::base::PlannerTerminationCondition &ptc)
+{
+  checkValidity();
+
+  return ompl::base::PlannerStatus::APPROXIMATE_SOLUTION;
+}
--- RIG.h
+++ RIG.h
@@ -1,7 +1,16 @@
 #ifndef RIG_
 #define RIG_
 
-class RIG {
+#include <ompl/base/Planner.h>
+#include <ompl/base/OptimizationObjective.h>
+#include <ompl/base/StateSampler.h>
+
+class RIG : ompl::base::Planner {
+public:
+    RIG(const ompl::base::SpaceInformationPtr &si);
+    virtual ~RIG();
+
+    virtual ompl::base::PlannerStatus solve(const ompl::base::PlannerTerminationCondition &ptc);
 };
 
 #endif
